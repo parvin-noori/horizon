901d1a9f60222a673eed55d56a4a987a139200fa
parvin-noori <parvinnoori0@gmail.com>
2025-09-15 14:02:18 +0330

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(accordion|divider).js",
+    "./node_modules/@heroui/theme/dist/components/*.js",
   ],
   theme: {
     extend: {},
