803573d00e7964253be19e39517d82659f5b42a6
parvin-noori <parvinnoori0@gmail.com>
2025-11-07 15:24:33 +0330

    add main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,13 @@
-import { Listbox, ListboxItem } from "@heroui/react";
+import {
+  BreadcrumbItem,
+  Breadcrumbs,
+  Button,
+  Input,
+  Listbox,
+  ListboxItem,
+} from "@heroui/react";
+import { FiSearch } from "react-icons/fi";
+import { LuBell } from "react-icons/lu";
 import {
   MdBarChart,
   MdGridView,
@@ -7,7 +16,7 @@ import {
   MdOutlineShoppingCart,
   MdPerson,
 } from "react-icons/md";
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useLocation } from "react-router";
 import getPro from "/imgs/GetPRO.svg";
 
 const sidebarItems = [
@@ -35,9 +44,12 @@ const sidebarItems = [
 ];
 
 export default function MainLayout() {
+  const location = useLocation();
+  const lastSegment = location.pathname.split("/").pop();
   return (
-    <div className="bg-secondary h-svh">
-      <div className="sidebar bg-white fixed h-full flex flex-col py-2">
+    <div className="bg-secondary h-svh flex">
+      {/* sidebar */}
+      <div className="sidebar bg-white  h-full flex flex-col py-2">
         <div className="sider-header p-12 border-b border-secondary">
           <span className="text-[#2B3674] uppercase text-2xl">
             <b>horizon </b>
@@ -69,14 +81,18 @@ export default function MainLayout() {
                     >
                       <span
                         className={`text-2xl transition-all duration-200 ${
-                          isActive ? "text-primary" : "text-slate-400 group-hover:text-primary"
+                          isActive
+                            ? "text-primary"
+                            : "text-slate-400 group-hover:text-primary"
                         }`}
                       >
                         {item.icon}
                       </span>
                       <span
                         className={`transition-all duration-200 ${
-                          isActive ? "text-[#2B3674]" : "text-slate-400 group-hover:text-primary"
+                          isActive
+                            ? "text-[#2B3674]"
+                            : "text-slate-400 group-hover:text-primary"
                         }`}
                       >
                         {item.name}
@@ -93,7 +109,36 @@ export default function MainLayout() {
           <img src={getPro} alt="Get Pro" className="mx-auto" />
         </div>
       </div>
-      <Outlet />
+      <div className="p-4 flex flex-col gap-y-4 py-16">
+        <header className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2 capitalize text-[#2B3674]">
+            <Breadcrumbs
+              itemClasses={{
+                separator: "px-2",
+              }}
+              separator="/"
+            >
+              <BreadcrumbItem>pages</BreadcrumbItem>
+              <BreadcrumbItem>{lastSegment}</BreadcrumbItem>
+            </Breadcrumbs>
+            <span className=" text-2xl font-semibold">{lastSegment}</span>
+          </div>
+          <div className="flex items-center">
+            <Input
+              startContent={<FiSearch />}
+              color="white"
+              placeholder="search"
+              size="md"
+              type="email"
+              radius="full"
+            />
+            <Button isOnlyIcon variant="light" radius="full">
+              <LuBell />
+            </Button>
+          </div>
+        </header>
+        <Outlet />
+      </div>
     </div>
   );
 }
