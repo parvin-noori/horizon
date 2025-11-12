4f8b616cb4f64428270855c5e26aced0120be56c
parvin-noori <parvinnoori0@gmail.com>
2025-11-12 12:42:56 +0330

    fix style

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ import LoginForm from "./loginForm";
 export default function Login() {
   return (
     <div className="lg:h-svh flex flex-col lg:gap-y-0 gap-y-10 md:justify-between p-5 max-w-lg">
-      <Link to="/" className="text-gray-400 text-sm flex items-center gap-x-2 group">
+      <Link to="/dashboard" className="text-gray-400 text-sm flex items-center gap-x-2 group">
         <IoChevronBackOutline className="group-hover:-translate-x-1 transition-transform duration-300" />
         Back to dashboard
       </Link>
