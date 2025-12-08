6e7b8d9223c73d1d2d3196df55df5a6561bfa5eb
parvin-noori <parvinnoori0@gmail.com>
2025-12-08 14:06:54 +0330

    update datePicker

--- src/features/dashboard/DatePick.jsx
+++ src/features/dashboard/DatePick.jsx
@@ -1,15 +1,10 @@
 // import DatePicker from "react-multi-date-picker";
-import {DatePicker} from "@heroui/date-picker";
-
+import { RangeCalendar } from "@heroui/react";
 export default function DatePick() {
   return (
-    <div className="bg-white dark:bg-secondary rounded-2xl shadow py-5 px-6 flex flex-col space-y-5">
-      {/* <DatePicker
-        range
-        dateSeparator=" تا "
-        calendarPosition="bottom-right"
-      /> */}
-      <DatePicker  open={true} className="max-w-[284px]" label="Birth date" />
-    </div>
+    <RangeCalendar
+      showMonthAndYearPickers
+      aria-label="Date (Show Month and Year Picker)"
+    />
   );
 }
