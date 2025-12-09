fe7bf02e916b2370fcdf66e98d4eaa18eb3f3242
parvin-noori <parvinnoori0@gmail.com>
2025-12-09 11:40:37 +0330

    update login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,25 @@
 import { IoChevronBackOutline } from "react-icons/io5";
 import { Link } from "react-router";
+import ThemeButton from "../../features/theme/ThemeButton";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
   return (
     <div className="lg:h-svh flex flex-col lg:gap-y-0 gap-y-10 md:justify-between p-5 max-w-lg">
-      <Link to="/dashboard" className="text-gray-400 text-sm flex items-center gap-x-2 group">
-        <IoChevronBackOutline className="group-hover:-translate-x-1 transition-transform duration-300" />
-        Back to dashboard
-      </Link>
+      <div className="flex items-cener justify-between">
+        <Link
+          to="/dashboard"
+          className="text-gray-400 text-sm flex items-center gap-x-2 group"
+        >
+          <IoChevronBackOutline className="group-hover:-translate-x-1 transition-transform duration-300" />
+          Back to dashboard
+        </Link>
+        <ThemeButton
+          className={
+            " hover:bg-primary lg:hidden hover:text-white transition-color duration-300 cursor-pointer  text-primary size-10 bg-secondary dark:bg-white/5 dark:text-white backdrop-blur-md grid place-content-center rounded-full"
+          }
+        />
+      </div>
       <LoginForm />
       <p className="text-gray-400 text-sm dark:text-white">
         © 2022 Horizon UI. All Rights Reserved. Made with love by Simmmple!
