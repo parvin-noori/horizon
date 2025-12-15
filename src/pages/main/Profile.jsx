ef5bf4ed190046e75d98067c6081c3f920b2c833
parvin-noori <parvinnoori0@gmail.com>
2025-12-15 21:56:09 +0330

    update data

--- src/pages/main/Profile.jsx
+++ src/pages/main/Profile.jsx
@@ -4,42 +4,11 @@ import Projects from "../../features/profile/Projects";
 import Storage from "../../features/profile/Storage";
 import Uploader from "../../features/profile/Uploader";
 import UserInfo from "../../features/profile/UserInfo";
-import userProfile from "/imgs/userImg.png";
-
-let userProfileInfo = {
-  avatar: userProfile,
-  name: "adela parkson",
-  jobPosition: "product designer",
-  posts: 17,
-  followers: 9.7,
-  following: 274,
-};
-
-let userInfo = {
-  education: "stanford university",
-  Languages: "English, Spanish,italian",
-  department: "product design",
-  workHistory: "google, facebook",
-  organization: "simmmple web LLC",
-  birthday: "20 July 1986",
-};
-
-const projects = [
-  {
-    name: "technology behind the blockchain",
-    img: "/imgs/project1.png",
-  },
-  {
-    name: "greatest way to good economy",
-    img: "/imgs/project2.png",
-  },
-  {
-    name: "most essential tips for burnout",
-    img: "/imgs/project3.png",
-  },
-];
+import { useGetData } from "../../hooks/useGetData";
 
 export default function Profile() {
+  const { data, isLoading, error } = useGetData();
+  const { userProfileInfo, projects, userInfo } = data ?? {};
   return (
     <div className="grid lg:grid-cols-12 gap-5">
       <div className="xl:col-span-5 col-span-6">
