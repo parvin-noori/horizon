e1d6dffd5f5565f7305b49d9506da54f2f8c1b19
parvin-noori <parvinnoori0@gmail.com>
2025-12-15 20:00:50 +0330

    update marketpLace

--- src/features/MarketPlace/MarketPlace.jsx
+++ src/features/MarketPlace/MarketPlace.jsx
@@ -1,21 +1,23 @@
+import { useRecentlyItems } from "../../hooks/useRecentlyItems";
 import { useTrendingItems } from "../../hooks/useTrendingItems";
 import Banner from "./Banner";
 import Creators from "./Creators";
 import History from "./History";
 import MarketPlaceItems from "./MarketPlaceItems";
 
-const { data: trendignItems } = useTrendingItems();
-const { data: recentlyItems } = recentlyItems();
-
-const allItems = [...trendignItems, ...recentlyItems];
-
 export default function MarketPlace() {
+  const { data: trendignItems, isLoading: loadingTrending } =
+    useTrendingItems();
+  const { data: recentlyItems, isLoading: loadingRecently } =
+    useRecentlyItems();
+
+  const allItems = [...(trendignItems ?? []), ...(recentlyItems ?? [])];
   return (
     <div className="grid lg:grid-cols-7 grid-cols-1 lg:gap-x-5 gap-y-5 lg:gap-y-0">
       <div className="lg:col-span-5 flex flex-col lg:space-y-15 space-y-10">
         <Banner />
-        <MarketPlaceItems title="Trending NFTs" items={trendignItems} />
-        <MarketPlaceItems title="Recently added" items={recentlyItems} />
+        <MarketPlaceItems title="Trending NFTs" items={trendignItems ?? []} />
+        <MarketPlaceItems title="Recently added" items={recentlyItems ?? []} />
       </div>
       <div className="lg:col-span-2 flex flex-col space-y-5">
         <Creators />
