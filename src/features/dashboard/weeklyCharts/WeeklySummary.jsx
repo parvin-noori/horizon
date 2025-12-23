9790b645b0748e2e800a04b0be83978f5aa60197
parvin-noori <parvinnoori0@gmail.com>
2025-12-23 11:06:49 +0330

    fix bug

--- src/features/dashboard/weeklyCharts/WeeklySummary.jsx
+++ src/features/dashboard/weeklyCharts/WeeklySummary.jsx
@@ -7,6 +7,9 @@ import { useGetData } from "../../../hooks/useGetData";
 export default function WeeklySummary() {
   const { data, isLoading, error } = useGetData();
   const { weeklySummary } = data ?? {};
+  if (isLoading) <span>loading..</span>;
+  if (error) console.log(error);
+  if (error) console.log(data);
   return (
     <div className="bg-white dark:bg-secondary rounded-2xl shadow py-5 px-6 flex flex-col space-y-5">
       <div className="flex items-center">
