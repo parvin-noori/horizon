502224674d9bac522ecd494b316230afaa54b9c1
parvin-noori <parvinnoori0@gmail.com>
2026-01-06 15:03:48 +0330

    update

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-function toCamelCase(str) {
+function toCamelCase(str: string) {
   if (typeof str !== "string") return "";
 
   return str
@@ -11,7 +11,7 @@ function toCamelCase(str) {
 export function useItemTranslation(basePath = "") {
   const { t } = useTranslation();
 
-  const translateItem = (title) => {
+  const translateItem = (title: string) => {
     const key = toCamelCase(title);
     return t(`${basePath}.${key}`, title);
   };
